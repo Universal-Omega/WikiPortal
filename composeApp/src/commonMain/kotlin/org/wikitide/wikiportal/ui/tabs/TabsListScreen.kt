4ed084cc97481760f89e96542263d193d29505db
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-25 23:32:12 -0600

    Fix insets

--- composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/tabs/TabsListScreen.kt
+++ composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/tabs/TabsListScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -52,6 +54,7 @@ fun TabsListScreen(
     Column(Modifier.fillMaxSize()) {
         TopAppBar(
             title = { Text("Tabs", style = MaterialTheme.typography.headlineMedium) },
+            windowInsets = TopAppBarDefaults.windowInsets.only(WindowInsetsSides.Top),
             colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background),
         )
 
