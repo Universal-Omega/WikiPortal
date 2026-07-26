f356ab038ff3bbf906bb9da2f2ca339cd8cfa77e
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-25 23:29:24 -0600

    Fix insets

--- composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/article/ArticleHostScreen.kt
+++ composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/article/ArticleHostScreen.kt
@@ -3,9 +3,12 @@ package org.wikitide.wikiportal.ui.article
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
@@ -32,6 +35,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -218,6 +222,7 @@ private fun SingleArticleTab(
     }
 
     Scaffold(
+        contentWindowInsets = WindowInsets(0.dp),
         topBar = {
             Column {
                 TopAppBar(
@@ -338,6 +343,7 @@ private fun SingleArticleTab(
                             )
                         }
                     },
+                    windowInsets = TopAppBarDefaults.windowInsets.only(WindowInsetsSides.Top),
                 )
 
                 if (pageState.isLoading) {
