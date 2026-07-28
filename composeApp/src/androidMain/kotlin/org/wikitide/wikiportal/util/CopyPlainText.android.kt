5f0876349651f39512e2f0505d357424f8973080
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-28 00:25:36 -0600

    Update

--- composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/util/CopyPlainText.android.kt
+++ composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/util/CopyPlainText.android.kt
@@ -1,9 +1,11 @@
 package org.wikitide.wikiportal.util
 
 import android.content.ClipData
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.Clipboard
-import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.toClipEntry
 
+@OptIn(ExperimentalComposeUiApi::class)
 actual suspend fun copyPlainText(clipboard: Clipboard, text: String): Boolean = runCatching {
-    clipboard.setClipEntry(ClipEntry(ClipData.newPlainText("WikiPortal logs", text)))
+    clipboard.setClipEntry(ClipData.newPlainText(text, text).toClipEntry())
 }.isSuccess
