e8985dc8716bfffa914d81117ac498b2783eb168
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-28 13:58:07 -0600

    Move materialIconsExtended to version catelog

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,8 +65,8 @@ kotlin {
         }
 
         commonMain.dependencies {
-            implementation(compose.materialIconsExtended)
             implementation(libs.compose.foundation)
+            implementation(libs.compose.material.icons.extended)
             implementation(libs.compose.material3)
             implementation(libs.compose.resources)
             implementation(libs.compose.runtime)
