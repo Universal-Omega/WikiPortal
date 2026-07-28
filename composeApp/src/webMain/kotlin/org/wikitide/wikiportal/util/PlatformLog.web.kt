c161bfa5f45884abc0ca17e519b99074f9a993ce
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-28 00:47:48 -0600

    Update

--- composeApp/src/webMain/kotlin/org/wikitide/wikiportal/util/PlatformLog.web.kt
+++ composeApp/src/webMain/kotlin/org/wikitide/wikiportal/util/PlatformLog.web.kt
@@ -1,11 +1,9 @@
 package org.wikitide.wikiportal.util
 
-import kotlinx.browser.console
-
 actual fun platformLog(level: LogLevel, tag: String, message: String) {
     val line = "[$tag] $message"
     when (level) {
-        LogLevel.DEBUG -> console.debug(line)
+        LogLevel.DEBUG -> console.log(line)
         LogLevel.INFO -> console.info(line)
         LogLevel.WARN -> console.warn(line)
         LogLevel.ERROR -> console.error(line)
