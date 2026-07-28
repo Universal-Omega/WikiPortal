f79821e5f735d621a8806f7ca289b9384a4ca2a6
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-28 13:55:13 -0600

    Update

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,8 +65,8 @@ kotlin {
         }
 
         commonMain.dependencies {
-            implementation(libs.androidx.compose.material.icons.extended)
             implementation(libs.compose.foundation)
+            implementation(libs.compose.material.icons.extended)
             implementation(libs.compose.material3)
             implementation(libs.compose.resources)
             implementation(libs.compose.runtime)
