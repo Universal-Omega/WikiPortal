89699719be93e0b7be7b2c2f12bf25ed25ccefb7
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-31 14:25:48 -0600

    Update

--- composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/util/AndroidAppVersionProvider.kt
+++ composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/util/AndroidAppVersionProvider.kt
@@ -4,7 +4,5 @@ import android.content.Context
 
 class AndroidAppVersionProvider(context: Context) : AppVersionProvider() {
     override val versionName: String =
-        context.packageManager.getPackageInfo(
-            context.packageName, 0
-        ).versionName ?: super.versionName
+        context.packageManager.getPackageInfo(context.packageName, 0).versionName ?: super.versionName
 }
