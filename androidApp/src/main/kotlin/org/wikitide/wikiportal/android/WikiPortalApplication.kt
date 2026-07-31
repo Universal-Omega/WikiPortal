34335c9806963d01934eec18cb8249fc94c985c0
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-31 15:03:35 -0600

    Add logging

--- androidApp/src/main/kotlin/org/wikitide/wikiportal/android/WikiPortalApplication.kt
+++ androidApp/src/main/kotlin/org/wikitide/wikiportal/android/WikiPortalApplication.kt
@@ -2,15 +2,16 @@ package org.wikitide.wikiportal.android
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.wikitide.wikiportal.di.appModules
+import org.wikitide.wikiportal.util.AppLogKoinLogger
 
 class WikiPortalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            logger(AppLogKoinLogger(Level.DEBUG))
             androidContext(this@WikiPortalApplication)
             modules(appModules())
         }
