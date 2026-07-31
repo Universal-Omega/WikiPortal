316df67824ba90337f0b9d39c5c6a8c548791541
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-31 14:16:21 -0600

    Remove

--- composeApp/src/iosMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.ios.kt
+++ composeApp/src/iosMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.ios.kt
@@ -5,11 +5,9 @@ import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import co.touchlab.sqliter.DatabaseConfiguration
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.wikitide.wikiportal.BuildKonfig
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
 import org.wikitide.wikiportal.data.store.WikiPortalStore
-import org.wikitide.wikiportal.util.AppVersionProvider
 import org.wikitide.wikiportal.util.IosOfflineArticleFileStore
 
 actual fun platformModule(): Module = module {
@@ -25,9 +23,4 @@ actual fun platformModule(): Module = module {
     }
     single<OfflineArticleFileStore> { IosOfflineArticleFileStore() }
     single<WikiPortalStore> { SqlDelightWikiPortalStore(get(), get()) }
-    single<AppVersionProvider> {
-        object : AppVersionProvider {
-            override val versionName = BuildKonfig.VERSION_NAME
-        }
-    }
 }
