70264e0c2084b289784ed65291206e221791ee67
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-31 13:59:25 -0600

    Add AppVersionProvider

--- composeApp/src/desktopMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.desktop.kt
@@ -4,9 +4,11 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.wikitide.wikiportal.BuildKonfig
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
 import org.wikitide.wikiportal.data.store.WikiPortalStore
+import org.wikitide.wikiportal.util.AppVersionProvider
 import org.wikitide.wikiportal.util.DesktopOfflineArticleFileStore
 import java.io.File
 
@@ -17,4 +19,9 @@ actual fun platformModule(): Module = module {
     }
     single<OfflineArticleFileStore> { DesktopOfflineArticleFileStore() }
     single<WikiPortalStore> { SqlDelightWikiPortalStore(get(), get()) }
+    single<AppVersionProvider> {
+        object : AppVersionProvider {
+            override val versionName = BuildKonfig.VERSION_NAME
+        }
+    }
 }
