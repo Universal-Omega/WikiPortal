295e586e716fefec66897af70d965a885b51910f
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-31 14:16:58 -0600

    Remove

--- composeApp/src/webMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.web.kt
+++ composeApp/src/webMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.web.kt
@@ -5,11 +5,9 @@ import app.cash.sqldelight.driver.worker.WebWorkerDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.w3c.dom.Worker
-import org.wikitide.wikiportal.BuildKonfig
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
 import org.wikitide.wikiportal.data.store.WikiPortalStore
-import org.wikitide.wikiportal.util.AppVersionProvider
 import org.wikitide.wikiportal.util.WebOfflineArticleFileStore
 import kotlin.js.ExperimentalWasmJsInterop
 
@@ -21,9 +19,4 @@ actual fun platformModule(): Module = module {
     single<SqlDriver> { WebWorkerDriver(createSqlJsWorker()) }
     single<OfflineArticleFileStore> { WebOfflineArticleFileStore(get()) }
     single<WikiPortalStore> { SqlDelightWikiPortalStore(get(), get()) }
-    single<AppVersionProvider> {
-        object : AppVersionProvider {
-            override val versionName = BuildKonfig.VERSION_NAME
-        }
-    }
 }
