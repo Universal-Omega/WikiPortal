067759d9e0d2dfac17758b01f5499fae7f01b5ed
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-07-31 13:58:05 -0600

    Add AppVersionProvider

--- composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
@@ -8,11 +8,13 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.wikitide.wikiportal.BuildKonfig
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
 import org.wikitide.wikiportal.data.store.WikiPortalStore
 import org.wikitide.wikiportal.util.AndroidLogExporter
 import org.wikitide.wikiportal.util.AndroidOfflineArticleFileStore
+import org.wikitide.wikiportal.util.AppVersionProvider
 import org.wikitide.wikiportal.util.LogExporter
 
 actual fun platformModule(): Module = module {
@@ -32,4 +34,13 @@ actual fun platformModule(): Module = module {
     single<LogExporter> { AndroidLogExporter(get()) }
     single<OfflineArticleFileStore> { AndroidOfflineArticleFileStore(get()) }
     single<WikiPortalStore> { SqlDelightWikiPortalStore(get(), get()) }
+    single<AppVersionProvider> {
+        val context = get<Context>()
+        val realVersionName = context.packageManager
+            .getPackageInfo(context.packageName, 0)
+            .versionName
+        object : AppVersionProvider {
+            override val versionName: String = realVersionName ?: BuildKonfig.VERSION_NAME
+        }
+    }
 }
