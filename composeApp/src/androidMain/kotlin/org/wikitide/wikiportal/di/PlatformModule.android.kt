5f19f224a84ea292b3247f7b1ade8d7775f0018a
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-01 13:29:35 -0600

    Fix

--- composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
@@ -2,6 +2,7 @@ package org.wikitide.wikiportal.di
 
 import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.db.synchronous
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
