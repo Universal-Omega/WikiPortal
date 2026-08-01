18094a0af408035b34e661f934895ead5e2f3cff
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-01 14:22:32 -0600

    Update

--- composeApp/src/iosMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.ios.kt
+++ composeApp/src/iosMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.ios.kt
@@ -2,23 +2,19 @@ package org.wikitide.wikiportal.di
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import co.touchlab.sqliter.DatabaseConfiguration
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
 import org.wikitide.wikiportal.data.store.WikiPortalStore
+import org.wikitide.wikiportal.db.WikiPortalDatabase
 import org.wikitide.wikiportal.util.IosOfflineArticleFileStore
 
 actual fun platformModule(): Module = module {
     single<SqlDriver> {
         NativeSqliteDriver(
-            DatabaseConfiguration(
-                name = "wikiportal.db",
-                version = 1,
-                create = { /* handled by SqlDelightWikiPortalStore.ensureSchema() */ },
-                upgrade = { _, _, _ -> },
-            ),
+            WikiPortalDatabase.Schema.synchronous(),
+            "wikiportal.db",
         )
     }
     single<OfflineArticleFileStore> { IosOfflineArticleFileStore() }
