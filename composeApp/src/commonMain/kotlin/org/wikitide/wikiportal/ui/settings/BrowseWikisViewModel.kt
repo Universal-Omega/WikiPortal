f0cd6d8d0fd6539c13765adae3c4abeb35cc3e44
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-02 17:13:50 -0600

    Add filter

--- composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/settings/BrowseWikisViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/settings/BrowseWikisViewModel.kt
@@ -18,12 +18,27 @@ class BrowseWikisViewModel(
     private val _searchQuery = MutableStateFlow("")
     val searchQuery: StateFlow<String> = _searchQuery
 
+    /** null means every language, otherwise one of IndieWikiSite.language's codes, see IndieWikiLanguages. */
+    private val _languageFilter = MutableStateFlow<String?>(null)
+    val languageFilter: StateFlow<String?> = _languageFilter
+
+    private val _officialOnly = MutableStateFlow(false)
+    val officialOnly: StateFlow<Boolean> = _officialOnly
+
     init {
         viewModelScope.launch { directory.ensureLoaded() }
     }
 
     fun setSearchQuery(query: String) {
         _searchQuery.value = query
+    }
+
+    fun setLanguageFilter(language: String?) {
+        _languageFilter.value = language
+    }
+
+    fun setOfficialOnly(officialOnly: Boolean) {
+        _officialOnly.value = officialOnly
     }
 
     fun refresh() {
