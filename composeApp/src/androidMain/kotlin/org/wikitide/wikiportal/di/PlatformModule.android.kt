00ee59482ab7a71d36b57768cbee0f950fe7ee1f
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-06 17:39:41 -0600

    Update
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
@@ -1,15 +1,14 @@
 package org.wikitide.wikiportal.di
 
 import android.content.Context
-import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
+import org.wikitide.wikiportal.data.store.WikiPortalSchema
 import org.wikitide.wikiportal.data.store.WikiPortalStore
-import org.wikitide.wikiportal.db.WikiPortalDatabase
 import org.wikitide.wikiportal.util.AndroidAppVersionProvider
 import org.wikitide.wikiportal.util.AndroidLogExporter
 import org.wikitide.wikiportal.util.AndroidOfflineArticleFileStore
@@ -19,7 +18,7 @@ import org.wikitide.wikiportal.util.LogExporter
 actual fun platformModule(): Module = module {
     single<SqlDriver> {
         AndroidSqliteDriver(
-            WikiPortalDatabase.Schema.synchronous(),
+            WikiPortalSchema,
             get<Context>(), "wikiportal.db",
         )
     }
