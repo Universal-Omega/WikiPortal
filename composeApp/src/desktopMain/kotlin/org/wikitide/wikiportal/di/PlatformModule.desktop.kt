8bca9b630b6e6b0d010e1dbfa134e091015b5b36
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-06 17:59:26 -0600

    Update
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- composeApp/src/desktopMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.desktop.kt
@@ -1,14 +1,13 @@
 package org.wikitide.wikiportal.di
 
-import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
+import org.wikitide.wikiportal.data.store.WikiPortalSchema
 import org.wikitide.wikiportal.data.store.WikiPortalStore
-import org.wikitide.wikiportal.db.WikiPortalDatabase
 import org.wikitide.wikiportal.util.DesktopOfflineArticleFileStore
 import java.io.File
 
@@ -17,7 +16,7 @@ actual fun platformModule(): Module = module {
         val dir = File(System.getProperty("user.home"), ".wikiportal").apply { mkdirs() }
         JdbcSqliteDriver(
             "jdbc:sqlite:${File(dir, "wikiportal.db").absolutePath}",
-            schema = WikiPortalDatabase.Schema.synchronous(),
+            schema = WikiPortalSchema,
         )
     }
     single<OfflineArticleFileStore> { DesktopOfflineArticleFileStore() }
