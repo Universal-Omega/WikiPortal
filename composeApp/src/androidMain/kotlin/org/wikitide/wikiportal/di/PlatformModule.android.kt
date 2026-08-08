ad744351276197e97c8517a29a15ddbfcdb80c97
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-08 17:18:04 -0600

    -
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/wikitide/wikiportal/di/PlatformModule.android.kt
@@ -5,7 +5,6 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.wikitide.wikiportal.data.PlatformLanguageSync
 import org.wikitide.wikiportal.data.store.OfflineArticleFileStore
 import org.wikitide.wikiportal.data.store.SqlDelightWikiPortalStore
 import org.wikitide.wikiportal.data.store.WikiPortalSchema
@@ -13,7 +12,6 @@ import org.wikitide.wikiportal.data.store.WikiPortalStore
 import org.wikitide.wikiportal.util.AndroidAppVersionProvider
 import org.wikitide.wikiportal.util.AndroidLogExporter
 import org.wikitide.wikiportal.util.AndroidOfflineArticleFileStore
-import org.wikitide.wikiportal.util.AndroidPlatformLanguageSync
 import org.wikitide.wikiportal.util.AppVersionProvider
 import org.wikitide.wikiportal.util.LogExporter
 
@@ -27,6 +25,5 @@ actual fun platformModule(): Module = module {
     single<AppVersionProvider> { AndroidAppVersionProvider(get()) }
     single<LogExporter> { AndroidLogExporter(get()) }
     single<OfflineArticleFileStore> { AndroidOfflineArticleFileStore(get()) }
-    single<PlatformLanguageSync> { AndroidPlatformLanguageSync(get()) }
     single<WikiPortalStore> { SqlDelightWikiPortalStore(get(), get()) }
 }
