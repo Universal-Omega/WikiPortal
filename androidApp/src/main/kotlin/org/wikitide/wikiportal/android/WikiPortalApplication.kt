6e8425f259996c5d4b41a7a4749a40e5b5bfcdb3
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-08 15:09:42 -0600

    Fix
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- androidApp/src/main/kotlin/org/wikitide/wikiportal/android/WikiPortalApplication.kt
+++ androidApp/src/main/kotlin/org/wikitide/wikiportal/android/WikiPortalApplication.kt
@@ -1,9 +1,13 @@
 package org.wikitide.wikiportal.android
 
 import android.app.Application
+import android.content.ComponentCallbacks2
+import android.content.res.Configuration
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import org.wikitide.wikiportal.data.AppRepository
 import org.wikitide.wikiportal.di.appModules
 import org.wikitide.wikiportal.util.AppLogKoinLogger
 
@@ -15,5 +19,17 @@ class WikiPortalApplication : Application() {
             androidContext(this@WikiPortalApplication)
             modules(appModules())
         }
+
+        registerComponentCallbacks(
+            object : ComponentCallbacks2 {
+                override fun onConfigurationChanged(newConfig: Configuration) {
+                    GlobalContext.get().get<AppRepository>().reconcilePlatformLanguage()
+                }
+
+                override fun onLowMemory() = Unit
+
+                override fun onTrimMemory(level: Int) = Unit
+            },
+        )
     }
 }
