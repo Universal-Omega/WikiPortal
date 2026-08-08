06d7b186e5fecc3badc590a6f1a7f5a7d1bd6a19
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-07 22:57:01 -0600

    Update
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/data/model/ThemeMode.kt
+++ composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/data/model/ThemeMode.kt
@@ -1,7 +1,11 @@
 package org.wikitide.wikiportal.data.model
 
-enum class ThemeMode(val label: String) {
-    SYSTEM("System default"),
-    LIGHT("Light"),
-    DARK("Dark"),
+import org.jetbrains.compose.resources.StringResource
+import org.wikitide.wikiportal.resources.Res
+
+/** [labelRes] is resolved with stringResource by the UI; the enum name itself is what's persisted. */
+enum class ThemeMode(val labelRes: StringResource) {
+    SYSTEM(Res.string.theme_mode_system),
+    LIGHT(Res.string.theme_mode_light),
+    DARK(Res.string.theme_mode_dark),
 }
