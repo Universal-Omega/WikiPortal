da24fd6cda818359f6d1c6f02ae5340d89cd8250
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-08 14:34:12 -0600

    Add
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/theme/AppLocale.kt
+++ composeApp/src/commonMain/kotlin/org/wikitide/wikiportal/ui/theme/AppLocale.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidedValue
 import androidx.compose.runtime.key
 
+@Composable
 expect fun languageNameInSystemLanguage(languageTag: String): String?
+
+expect val appLocaleRemountRequired: Boolean
 
 expect object LocalAppLocale {
     val current: String
@@ -18,7 +21,11 @@ expect object LocalAppLocale {
 @Composable
 fun AppLocaleEnvironment(languageTag: String?, content: @Composable () -> Unit) {
     CompositionLocalProvider(LocalAppLocale provides languageTag) {
-        key(languageTag) {
+        if (appLocaleRemountRequired) {
+            key(languageTag) {
+                content()
+            }
+        } else {
             content()
         }
     }
