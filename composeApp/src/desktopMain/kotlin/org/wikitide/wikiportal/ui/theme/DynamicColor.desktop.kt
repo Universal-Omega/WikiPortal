24880784ad5adfe88991ce32449bc143b783d7da
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-07 18:12:26 -0600

    ReadOnlyComposable
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- composeApp/src/desktopMain/kotlin/org/wikitide/wikiportal/ui/theme/DynamicColor.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/wikitide/wikiportal/ui/theme/DynamicColor.desktop.kt
@@ -2,8 +2,10 @@ package org.wikitide.wikiportal.ui.theme
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 
 actual fun dynamicColorSchemeAvailable(): Boolean = false
 
 @Composable
+@ReadOnlyComposable
 actual fun platformDynamicColorScheme(useDark: Boolean): ColorScheme? = null
